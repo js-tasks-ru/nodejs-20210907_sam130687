8992216bef5fe4d8b417210366900047ab1cde5a
DESKTOP-G6TI56S\Саламат <Sam130687@gmail.com>
2021-10-04 08:24:06 +0300

    solve 6 lessone

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,29 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const category = await Category.find({});
+
+  if (!category) {
+    ctx.throw(404, 'Category empty');
+  };
+
+  const resultBody = category.map(categoryItem => {
+    const resultCategory = {
+      id : categoryItem._id,
+      title : categoryItem.title
+    };
+    
+    if (categoryItem.subcategories) {
+      resultCategory.subcategories = categoryItem.subcategories.map(
+        (subCategoryItem) => ({
+            id : subCategoryItem._id,
+            title : subCategoryItem.title
+          })
+        );
+    };
+
+    return resultCategory;  
+  });
+
+  ctx.body = {categories: resultBody};
 };
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,64 @@
+const ObjectId = require('mongoose').Types.ObjectId;
+const Product = require('../models/Product');
+
+function formatProduct(product) {
+  return {
+    id: product._id,
+    title: product.title,
+    price: product.price,
+    images: product.images,
+    category: product.category,
+    description: product.description,
+    subcategory: product.subcategory
+  }
+};
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
   ctx.body = {};
+
+  const products = await Product.find({subcategory : subcategory});
+  if (products) {
+    const resultBody = products.map(productItem => {
+      return formatProduct(productItem);  
+    }); 
+
+    ctx.body = {products: resultBody};  
+  };  
 };
 
 module.exports.productList = async function productList(ctx, next) {
+  const products = await Product.find({});
+
   ctx.body = {};
+
+  if (products) {
+    const resultBody = products.map(productItem => {
+      return formatProduct(productItem);  
+    });   
+    
+    ctx.body = {products: resultBody};
+  };  
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const productId = ctx.params.id;
+
+  if (!ObjectId.isValid(productId)) {
+    ctx.status = 400;
+    return;
+  };
+
+  const findProduct = await Product.findOne({_id : productId});
+
+  if (!findProduct) {
+    ctx.status = 404;
+    return;
+  };  
+      
+  ctx.body = {product : formatProduct(findProduct)};
 };
 

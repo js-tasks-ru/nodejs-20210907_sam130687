5c5e77dda1b8f120d162229e02c69278ffdfa9f0
DESKTOP-G6TI56S\Саламат <Sam130687@gmail.com>
2021-10-07 20:52:11 +0300

    solve 7 lesson

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,33 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      if (!email) {
+        return done(null, false, 'Необходимо указать E-mail');
+      };
+
+      if (!password) {
+        return done(null, false, 'Необходимо указать пароль');
+      };
+
+      try {
+        const findUser = await User.findOne({ email });
+
+        if (!findUser){
+          return done(null, false, 'Нет такого пользователя');
+        };
+
+        const isPassValid = await findUser.checkPassword(password);
+
+        if (isPassValid){
+          done(null, findUser); 
+        } else {
+          done(null, false, 'Неверный пароль');
+        };        
+      } catch (error) {
+        done(error);
+      };
     },
 );
--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,26 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  };
+
+  try {
+    const findUser = await User.findOne({ email });
+
+    if (findUser){
+      done(null, findUser); 
+    } else
+    {
+      try {
+        const newUser = await User.create({email, displayName});  
+        return done(null, newUser);         
+      } catch (error) {
+        return done(error, false, error.message);
+      };
+    }; 
+      
+  } catch (error) {
+    done(error);
+  };
 };

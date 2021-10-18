49494fe22ffd2519ebfe32481af7a89dd2f5daf2
DESKTOP-G6TI56S\Саламат <Sam130687@gmail.com>
2021-10-18 10:53:40 +0300

    solve lesson 9

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,20 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
+  const messages = await Message.find({chat: ctx.user}).limit(20);
   ctx.body = {};
+
+  if (messages) {
+    const resultBody = messages.map((message) => {
+      return {
+        date: message.date,
+        text: message.text,
+        id: message._id,
+        user: message.user
+      };  
+    }); 
+
+    ctx.status = 200;
+    ctx.body = {messages: resultBody};  
+  };   
 };
